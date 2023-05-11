068a100e86cfd720043cbc110c03e70ec9fa4400
Lii02 <lukeinlow@gmail.com>
2023-05-11 19:33:02 -0400

    Started python API

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="LiStorage",
+    version="1.0",
+    author="Li",
+    install_requires=[
+        "certifi>=2023.5.7",
+        "charset-normalizer==3.1.0",
+        "urllib3>=2.0.2"
+    ],
+)
