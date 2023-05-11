af54c3c75983b097a6d9c2bfac2e5b92f0f8ff9e
Lii02 <lukeinlow@gmail.com>
2023-05-11 11:13:34 -0400

    Cleaned up main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from flask import *
 from flask_cors import *
 from flask_restful import *
-import resources
+from resources import *
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-api.add_resource(resources.Refresh, "/refresh")
-api.add_resource(resources.CreateBucket, "/create_bucket")
-api.add_resource(resources.RemoveBucket, "/remove_bucket")
-api.add_resource(resources.UploadFile, "/upload")
-api.add_resource(resources.DownloadFile, "/download")
+api.add_resource(Refresh, "/refresh")
+api.add_resource(CreateBucket, "/create_bucket")
+api.add_resource(RemoveBucket, "/remove_bucket")
+api.add_resource(UploadFile, "/upload")
+api.add_resource(DownloadFile, "/download")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6250, debug=True)
