e2b03021d0e2802db67d69eacbf1e19301a1998d
Lii02 <lukeinlow@gmail.com>
2023-05-15 21:48:51 -0400

    Functional get and post requests in client.py

--- python/lss/client.py
+++ python/lss/client.py
@@ -1,22 +1,26 @@
-import urllib3
+import requests
+import json
 from .header import *
-from typing import Dict
+from typing import Dict, Any
 
 class Client:
     def __init__(self, address: str, access_key: str, password_key: str):
-        self.pool = urllib3.PoolManager()
         self.address = address
         self.access_key = access_key
         self.password_key = password_key
 
     def authenticate(self):
-        self._post_request("/authenticate", create_header(ContentType.JSON))
+        data = {
+            "access_key": self.access_key,
+            "password_key": self.password_key
+        }
+        self._post_request("/authenticate", create_header(ContentType.JSON), data)
 
     def deauthenticate(self):
         self._post_request("/deauthenticate", create_header(ContentType.NONE))
 
-    def _get_request(self, route: str, header: Dict[str, str]):
-        pass
+    def _get_request(self, route: str, header: Dict[str, str], payload: Dict[str, Any] = None):
+        return requests.get(f"{self.address}/{route}", headers=header, json=payload)
 
-    def _post_request(self, route: str, header: Dict[str, str]):
-        pass
+    def _post_request(self, route: str, header: Dict[str, str], payload: Dict[str, Any] = None):
+        requests.post(f"{self.address}/{route}", headers=header, json=payload)
--- python/setup.py
+++ python/setup.py
@@ -5,8 +5,5 @@ setup(
     version="1.0",
     author="Li",
     install_requires=[
-        "certifi>=2023.5.7",
-        "charset-normalizer==3.1.0",
-        "urllib3>=2.0.2"
     ],
 )
